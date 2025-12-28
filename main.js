8b3c2de619b0dc90e265d288e7a4d8ebf6c8f8b0
mohamedsamy2026 <mohamedeng747@gmail.com>
2025-12-28 13:46:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+  Task:
+    - When click to humburger menu show/hide navbar also toggle between bars/x-mark
+*/
+
+const navbar = document.querySelector(".navbar");
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector(".fa-xmark");
+const humburgerMenu = document.querySelector(".humburger");
+
+humburgerMenu.addEventListener("click", () => {
+  bars.classList.toggle("active");
+  xmark.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
